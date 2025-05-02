4373e7d851797636e09f02beb06829a7e44d1e68
OseniMayokun <osenimayokun@g.com>
2025-05-02 07:32:51 +0100

    Recipe Website Depolyment

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ea6a9eea-77c1-488a-b899-57d7e3d4b3d3';
+export const MODAL_CLOSE_SEC = 2.5;
